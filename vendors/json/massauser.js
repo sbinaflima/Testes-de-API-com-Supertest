532420af9d6dac973833493826ce92a79c3b5ec6
sbinaflima <sbinaflima@gmail.com>
2025-08-13 21:43:26 -0300

    Add arquivos no repo

---
+++ vendors/json/massauser.js
@@ -0,0 +1,20 @@
+module.exports.array = [
+ {
+   "nomeUser": "sabrina",
+   "userId": 9860,
+   "firstName": "fonseca",
+   "lastName": "lima"
+ },
+ {
+   "nomeUser": "Jean",
+   "userId": 9230,
+   "firstName": "rodrigues",
+   "lastName": "santos"
+ },
+ {
+   "nomeUser": "milena",
+   "userId": 9125,
+   "firstName": " alves",
+   "lastName": " lopes"
+ }
+]
